49c9831b98cae39e1499eff4d7fee0e0c440511e
Faisal Yudha Nugraha <89179669+yudhaCode@users.noreply.github.com>
2024-03-18 14:34:30 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Random Quote Generator
+
+This project is a simple random quote generator that fetches quotes from the [type.fit](https://type.fit/api/quotes) A
+
+## Technologies Used
+
+- HTML
+- CSS
+
+## Features
+
+- Display random quotes on the webpage
+- Copy quote to clipboard functionality
---
+++ script/script.js
@@ -0,0 +1,57 @@
+// Define fetchQuote function outside the DOMContentLoaded event listener
+function fetchQuote(url) {
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const quote = data[randomIndex];
+      document.querySelector(".header h2").textContent = `- ${quote.author}`;
+      document.querySelector(".content p").textContent = `"${quote.text}"`;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const btnGenerate = document.querySelector(".btn-generate");
+  const btnCopy = document.querySelector(".btn-copy");
+  const btnFamous = document.querySelector(".btn-famous");
+  const btnInspirational = document.querySelector(".btn-inspirational");
+
+  btnGenerate.addEventListener("click", function () {
+    fetch("https://type.fit/api/quotes")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const quote = data[randomIndex];
+        document.querySelector(".header h2").textContent = `${quote.author}`;
+        document.querySelector(".content p").textContent = `"${quote.text}"`;
+      });
+  });
+
+  btnCopy.addEventListener("click", function () {
+    const quoteToCopy = `${document.querySelector(".header h2").textContent}\n${
+      document.querySelector(".content p").textContent
+    }`;
+    navigator.clipboard
+      .writeText(quoteToCopy)
+      .then(() => {
+        alert("Quote copied to clipboard!");
+      })
+      .catch((error) => {
+        console.error("Failed to copy: ", error);
+      });
+  });
+
+  btnFamous.addEventListener("click", function () {
+    fetchQuote("https://type.fit/api/quotes?category=famous");
+  });
+
+  btnInspirational.addEventListener("click", function () {
+    fetchQuote("https://type.fit/api/quotes?category=inspirational");
+  });
+});
